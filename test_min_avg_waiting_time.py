fefbfcd79a421a2630754409be3ea57c64713185
Douglas Hellinger <doug.hellinger@hotmail.com>
2018-04-28 22:48:10 +0800

    improve RR min avg waiting time algorithm

--- test_min_avg_waiting_time.py
+++ test_min_avg_waiting_time.py
@@ -9,31 +9,31 @@ class AverageWaitingTimeTest(unittest.TestCase):
 
     def test_RR_min_avg_waiting_time(self):
         '''Find the time quantum which produces the minimum average waiting time for RR.'''
+        results = []
 
-        min_avg_waiting_time = 100000
-
-        for quantum in range(1, 11):
+        for quantum in range(1, 20):
             (schedule, avg_waiting_time) = RR_scheduling(self.process_list, quantum)
-            if avg_waiting_time < min_avg_waiting_time:
-                min_avg_waiting_time = avg_waiting_time
+            results.append((quantum, avg_waiting_time, schedule))
+
+        quantum, avg_waiting_time, schedule = min(results, key=lambda r: r[1])
 
         print self._testMethodDoc
         print "quantum: ", quantum,
         print ", avg_waiting_time: ", avg_waiting_time
         print "schedule: ", schedule
         print "-"*80
-        self.assertEquals(6.4375, min_avg_waiting_time)
+        self.assertEquals(6.4375, avg_waiting_time)
 
     def test_SJF_min_avg_waiting_time(self):
-        '''
-        Find the historical predicted burst_time weighting (alpha) which produces the minimum
-        average waiting time for SJF.'''
+        '''Find the historical predicted burst_time weighting (alpha) which produces the minimum average waiting time for SJF.'''
         results = []
         for alpha in [x/10.0 for x in range(0,10)]:
             (schedule, avg_waiting_time) = SJF_scheduling(self.process_list, alpha)
             results.append((alpha, avg_waiting_time, schedule))
 
         alpha, min_avg_waiting_time, schedule = min(results, key=lambda r: r[1])
+
+        print self._testMethodDoc
         print "alpha:", alpha,
         print "min_avg_waiting_time:", min_avg_waiting_time
         print "schedule:", schedule
