554138a2929012e4c7ddfb749aa9f560693501c4
Douglas Hellinger <doug.hellinger@hotmail.com>
2018-04-28 22:16:18 +0800

    find the time quantum for min avg waiting time in RR

---
+++ test_min_avg_waiting_time.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import unittest
+from simulator import *
+
+class AverageWaitingTimeTest(unittest.TestCase):
+
+    def setUp(self):
+        self.process_list = read_input('input.txt')
+
+    def test_RR_min_avg_waiting_time(self):
+        '''Find the time quantum which produces the minimum average waiting time for RR.'''
+
+        min_avg_waiting_time = 100000
+
+        for quantum in range(1, 11):
+            (schedule, avg_waiting_time) = RR_scheduling(self.process_list, quantum)
+            if avg_waiting_time < min_avg_waiting_time:
+                min_avg_waiting_time = avg_waiting_time
+
+        print "quantum: " + str(quantum),
+        print ", avg_waiting_time: " + str(avg_waiting_time)
+        print schedule
+        self.assertEquals(6.4375, min_avg_waiting_time)
+
+if __name__ == '__main__':
+    unittest.main()
